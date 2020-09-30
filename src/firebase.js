a90dd6677dd6c771dc65d9ecb4d2370b8a01deaa
Lokindra Dangi <lokendradangi0@gmail.com>
2020-09-30 22:19:09 +0545

    firebase is setup

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCx73qjzRsKUHK_BEbnEwiIH4tkp-R4Jdk",
+  authDomain: "facebook-clone-f8b69.firebaseapp.com",
+  databaseURL: "https://facebook-clone-f8b69.firebaseio.com",
+  projectId: "facebook-clone-f8b69",
+  storageBucket: "facebook-clone-f8b69.appspot.com",
+  messagingSenderId: "1067758270357",
+  appId: "1:1067758270357:web:47a02957bbe2590bf5689c",
+};
+
+//initalize firebase App
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+//Accesing Database
+const db = firebaseApp.firestore();
+
+export default db;
