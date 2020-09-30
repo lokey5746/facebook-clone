dbd5fd3da3cdc29be0c53c6232a2821094c9577b
Lokindra Dangi <lokendradangi0@gmail.com>
2020-09-30 21:54:19 +0545

    app cleanup is Done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h2>let's Build a facebook clone</h2>
+    </div>
+  );
+}
+
+export default App;
