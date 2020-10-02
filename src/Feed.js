de970c5e293790c3d4cdc99b385b0d318a6d3205
Lokindra Dangi <lokendradangi0@gmail.com>
2020-10-02 15:40:35 +0545

    post frontend is complete

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Feed.css";
 import MessageSender from "./MessageSender";
+import Post from "./Post";
 import StoryReel from "./StoryReel";
 
 function Feed() {
@@ -10,6 +11,13 @@ function Feed() {
       <StoryReel />
       {/* MessageSender */}
       <MessageSender />
+      {/* post */}
+      <Post
+        profilePic="https://m.media-amazon.com/images/M/MV5BNmVkODYwNDgtY2I2MC00Mjg4LWI4OTUtNzFhYTA3ODJhOTA2XkEyXkFqcGdeQXVyNDc2NzU1MTA@._V1_SY1000_CR0,0,743,1000_AL_.jpg"
+        username="Pragya Jaiswal"
+        timestamp="20 minute ago."
+        image="https://pbs.twimg.com/profile_images/1253051510809919490/5rKWySbh_400x400.jpg"
+      />
     </div>
   );
 }
