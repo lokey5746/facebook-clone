bde2f05188dcda71e02831d85998543947870a93
Lokindra Dangi <lokendradangi0@gmail.com>
2020-10-02 21:21:38 +0545

    widgets is complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Sidebar />
         {/* Feed */}
         <Feed />
-        {/* Widgets */}
+        <Widgets />
       </div>
     </div>
   );
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Widgets.css";
+import { FacebookProvider, Page } from "react-facebook";
 
 function Widgets() {
-  return <div className="widgets"></div>;
+  return (
+    <div className="widgets">
+      <FacebookProvider appId="rohan.dangi.31">
+        <Page
+          href="https://www.facebook.com/Marvel-Fanclub-Nepal-2296318483968802"
+          tabs="timeline"
+        />
+      </FacebookProvider>
+    </div>
+  );
 }
 
 export default Widgets;
